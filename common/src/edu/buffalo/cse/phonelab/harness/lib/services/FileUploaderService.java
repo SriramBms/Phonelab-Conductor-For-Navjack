13cacbf5968b757244b9efc9d59b35622420dfd8
Jinghao Shi <jinghaos@buffalo.edu>
2013-08-14 15:10:21 -0400

    Starting convert file uploader to a service

--- common/src/edu/buffalo/cse/phonelab/harness/lib/services/FileUploaderService.java
+++ common/src/edu/buffalo/cse/phonelab/harness/lib/services/FileUploaderService.java
@@ -1,4 +1,4 @@
-package edu.buffalo.cse.phonelab.harness.lib.tasks;
+package edu.buffalo.cse.phonelab.harness.lib.services;
 
 import java.io.File;
 import java.io.StringWriter;
@@ -32,17 +32,13 @@ import edu.buffalo.cse.phonelab.harness.lib.periodictask.PeriodicTask;
 import edu.buffalo.cse.phonelab.harness.lib.services.UploaderService;
 import edu.buffalo.cse.phonelab.harness.lib.services.UploaderService.LoggerBinder;
 
-public class FileUploaderTask extends PeriodicTask<FileUploaderParameters, FileUploaderState> implements UploaderClient {
+public class FileUploaderService extends Service implements UploaderClient {
     private final String TAG = "PhoneLabServices-" + this.getClass().getSimpleName();
 
     private final String PREFS_NAME = this.getClass().getName() + ".FILES_TO_UPLOAD";
     private final String PREFS_KEY = this.getClass().getName() + ".PATH_LIST";
 
-    public final String ACTION_UPLOAD = this.getClass().getName() + ".UPLOAD";
-    public final String ACTION_UPLOAD_COMPLETED = this.getClass().getName() + ".UPLOAD_COMPLETED";
-    
-    public final String BUNDLE_KEY_PATH_LIST = this.getClass().getName() + ".PATH_LIST";
-    public final String BUNDLE_KEY_PACKAGENAME = this.getClass().getName() + ".PACKAGENAME";
+    private final int MSG_UPLOAD_FILE = 1;
 
     private Set<UploaderFileDescription> uploadFiles;
     private SharedPreferences sharedPreferences;
@@ -50,45 +46,45 @@ public class FileUploaderTask extends PeriodicTask<FileUploaderParameters, FileU
     private FileUploaderReceiver fileUploaderReceiver;
     private UploaderService uploaderService;
 
+    private started = false;
+
     private ServiceConnection uploaderServiceConnection = new ServiceConnection() {
         
         @Override
         public void onServiceConnected(ComponentName className, IBinder iBinder) {
-            Log.v(TAG, "Connecting to uploader service.");
+            Log.v(TAG, "Connected to uploader service.");
             LoggerBinder binder = (LoggerBinder) iBinder;
             uploaderService = binder.getService();
-            uploaderService.registerLogger(FileUploaderTask.this, UploaderService.PRIORITY_LOW);
+            uploaderService.registerLogger(FileUploaderService.this, UploaderService.PRIORITY_LOW);
         }
         
         @Override
         public void onServiceDisconnected(ComponentName arg0) {
-            Log.w(TAG, "Uploader service disconnected.");
+            Log.w(TAG, "Disconnected to uploader service.");
             persistUploadFileList();
         }
     };
     
-    public FileUploaderTask(Context context) {
-        super(context, "FileUploaderService");
-        this.context = context;
-
+	@Override
+	public synchronized int onStartCommand(Intent intent, int flags, int startId) {
+		if (started == true) {
+			Log.v(TAG, "Not restarting running service.");
+			return START_STICKY;
+		}
+		
+        Log.v(TAG, "-------------- STARTING FILE UPLOADER SERVICE ---------------");
+		super.onStartCommand(intent, flags, startId);
+	
         uploadFiles = Collections.synchronizedSet(new HashSet<UploaderFileDescription>());
         sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 
         recoverUploadFileList();
         
-        context.bindService(new Intent(context, UploaderService.class), uploaderServiceConnection, Context.BIND_AUTO_CREATE);
-
-        IntentFilter intentFilter = new IntentFilter(ACTION_UPLOAD);
-        fileUploaderReceiver = new FileUploaderReceiver();
-        context.registerReceiver(fileUploaderReceiver, intentFilter);
+        bindService(new Intent(this, UploaderService.class), uploaderServiceConnection, Context.BIND_AUTO_CREATE);
+        
+        started = true;
     }
         
-    @Override
-    protected void check(FileUploaderParameters parameters) {
-        updateUploadFiles();
-        Log.v(TAG, uploadFiles.size() + " files to be uploaded");
-    }
-
     private void updateUploadFiles() {
     	
         state.bytesAvailable = 0;
@@ -154,26 +150,6 @@ public class FileUploaderTask extends PeriodicTask<FileUploaderParameters, FileU
     }
 
     @Override
-    public FileUploaderParameters newParameters() {
-        return new FileUploaderParameters();
-    }
-
-    @Override
-    public FileUploaderParameters newParameters(FileUploaderParameters parameters) {
-        return new FileUploaderParameters(parameters);
-    }
-
-    @Override
-    public FileUploaderState newState() {
-        return new FileUploaderState();
-    }
-
-    @Override
-    public Class<FileUploaderParameters> parameterClass() {
-        return FileUploaderParameters.class;
-    }
-
-    @Override
     public long bytesAvailable() {
         return state.bytesAvailable;
     }
@@ -182,14 +158,18 @@ public class FileUploaderTask extends PeriodicTask<FileUploaderParameters, FileU
     public void complete(UploaderFileDescription uploaderFileDescription,
             boolean success) {
         if (success) {
-            String path = uploaderFileDescription.src;
-            Intent intent = new Intent(ACTION_UPLOAD_COMPLETED);
-            ArrayList<String> completedFileList = new ArrayList<String>();
-            completedFileList.add(path);
-            intent.putStringArrayListExtra(BUNDLE_KEY_PATH_LIST, completedFileList);
-            Log.v(TAG, "Broadcasting upload complete intent for file " + path);
-            context.sendBroadcast(intent);
-
+            File file = new File(uploaderFileDescription.src);
+            /* truncate sent files */
+            if (file.canWrite()) {
+                try {
+                    RandomAccessFile raf = new RandomAccessFile(file, "rw");
+                    raf.setLength(0);
+                    raf.close();
+                }
+                catch (Exception e) {
+                    Log.w("Failed to truncate sent file " + file.getName() + ": " + e);
+                }
+            }
             uploadFiles.remove(path);
             persistUploadFileList();
         }
@@ -219,115 +199,39 @@ public class FileUploaderTask extends PeriodicTask<FileUploaderParameters, FileU
         updateUploadFiles();
     }
 
-    @Override
-    public synchronized void stop() {
-        persistUploadFileList();
-        context.unregisterReceiver(fileUploaderReceiver);
-        super.stop();
-    }
-
-    @Override
-    public String getState() {
-        updateUploadFiles();
-        return super.getState();
-    }
-
-    class FileUploaderReceiver extends BroadcastReceiver {
+    class MessageHandler extends Handler {
 
         @Override
-        public void onReceive(Context context, Intent intent) {
-        	
-            if (intent.getAction().equals(ACTION_UPLOAD)) {
-                Bundle bundle = intent.getExtras();
-                String packagename;
-                ArrayList<String> paths;
-                
-                if (bundle.containsKey(BUNDLE_KEY_PACKAGENAME)) {
-                	packagename = bundle.getString(BUNDLE_KEY_PACKAGENAME);
-                } else {
-                	Log.e(TAG, "Bundle doesn't contain packagename.");
-                	return;
-                }
-                
-                if (bundle.containsKey(BUNDLE_KEY_PATH_LIST)) {
-                    paths = bundle.getStringArrayList(BUNDLE_KEY_PATH_LIST);
-                } else {
-                	Log.e(TAG, "Bundle doesn't contain file list.");
-                	return;
-                }
-                
-                for (String path : paths) {
-                	UploaderFileDescription uploadFile;
-                	try {
-                		uploadFile = new UploaderFileDescription(path, new File(path).getName(), packagename);
-                	} catch (Exception e) {
-                		Log.w(TAG, "Couldn't create upload file from path " + path + ": " + e);
-                		continue;
-                	}
-                	uploadFiles.add(uploadFile);
-                }
-                
-                persistUploadFileList();
-                return;
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_UPLOAD_FILE: 
+                    ArrayList<String> content = (ArrayList<String>)msg.obj;
+                    String packageName = content.get(0);
+                    ArrayList<String> paths = content.subList(1, content.size());
+
+                    for (String path : paths) {
+                        UploaderFileDescription uploadFile;
+                        try {
+                            /* TODO 
+                             * may need to further check if pathName in
+                             * path
+                             */
+                            uploadFile = new UploaderFileDescription(path, (new File(path)).getName(), packageName);
+                        } catch (Exception e) {
+                            Log.w(TAG, "Couldn't create upload file from path " + path + ": " + e);
+                            continue;
+                        }
+                        uploadFiles.add(uploadFile);
+                    }
+                    
+                    persistUploadFileList();
             }
         }
     }
-}
 
-@Root(name="FileUploaderService")
-class FileUploaderParameters extends PeriodicParameters {
+    final Messenger messenger = new Messenger(new MessageHandler());
 
-    /**
-     *
-     */
-    public FileUploaderParameters() {
-        super();
-
-    }
-
-    public FileUploaderParameters(FileUploaderParameters parameters) {
-        super(parameters);
-
-    }
-
-    public String toString() {
-        return this.getClass().getSimpleName() + "[]";
-    }
-
-    public int hashCode() {
-        // final int prime = 31;
-        int result = super.hashCode();
-
-        return result;
-    }
-
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (!super.equals(obj))
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        // FileUploaderParameters other = (FileUploaderParameters) obj;
-
-
-        return true;
-    }
-}
-
-@Root(name="FileUploaderService")
-class FileUploaderState extends PeriodicState {
-    @Element
-    public long bytesAvailable;
-
-    @Element
-    public int filesAvailable;
-    /**
-     *
-     */
-    public FileUploaderState() {
-        super();
-        bytesAvailable = 0L;
-        filesAvailable = 0;
+    public IBinder onBind(Intent intent) {
+        return messenger.getBinder();
     }
 }
